feb10d2aa2e5289b1a50867fe14681ca67928d84
Abdessamad Chalouchi <65370748+abdessamad-chalouchi@users.noreply.github.com>
2025-05-05 23:48:33 +0100

    Time: 39 ms (41.69%), Space: 63.3 MB (14.83%) - LeetHub

--- 1464-reduce-array-size-to-the-half/1464-reduce-array-size-to-the-half.java
+++ 1464-reduce-array-size-to-the-half/1464-reduce-array-size-to-the-half.java
@@ -1,21 +1,36 @@
 class Solution {
+    // public int minSetSize(int[] arr) {
+    //     int n = arr.length;
+    //     // if (n ==2){
+    //     //     return 1;
+    //     // }
+    //     HashMap<Integer, Integer> map = new HashMap<>();
+    //     for (int j : arr) {
+    //         map.put(j, map.getOrDefault(j, 0) + 1);
+    //     }
+    //     List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+    //     list.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+    //     int i = 0;
+    //     int sum = 0;
+    //     while (sum < (n/2)) {
+    //         sum += list.get(i).getValue();
+    //         i++;
+    //     }
+    //     return i;
+    // }
     public int minSetSize(int[] arr) {
         int n = arr.length;
-        // if (n ==2){
-        //     return 1;
-        // }
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int j : arr) {
-            map.put(j, map.getOrDefault(j, 0) + 1);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
-        list.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
-        int i = 0;
-        int sum = 0;
-        while (sum < (n/2)) {
-            sum += list.get(i).getValue();
-            i++;
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        maxHeap.addAll(map.values());
+        int removed = 0, setSize = 0;
+        while (removed < n / 2) {
+            removed += maxHeap.poll();
+            setSize++;
         }
-        return i;
+        return setSize;
     }
 }
