b270d7aa03bb922519933fee3edf95528d5ea60b
Abdessamad Chalouchi <65370748+abdessamad-chalouchi@users.noreply.github.com>
2025-05-14 14:13:38 +0100

    Time: 4 ms (20.08%), Space: 43.2 MB (11.49%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        if (a.length() > b.length()) {
+            b = "0".repeat(a.length() - b.length()) + b;
+        } else {
+            a = "0".repeat(b.length() - a.length()) + a;
+        }
+        int tmp = 0;
+        for (int i = a.length() - 1; i >= 0; i--) {
+            int r = Integer.parseInt(a.charAt(i) + "") + Integer.parseInt(b.charAt(i) + "") + tmp;
+            if (r == 2) {
+                tmp = 1;
+                result.append("0");
+            } else if (r == 3) {
+                tmp = 1;
+                result.append("1");
+            } else {
+                tmp = 0;
+                result.append(r);
+            }
+        }
+        if (tmp != 0) {
+            result.append(tmp);
+        }
+        return result.reverse().toString();
+    }
+}
