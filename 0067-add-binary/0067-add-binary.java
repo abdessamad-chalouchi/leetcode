912c4b80c36198dde678954a2bc001322944d020
Abdessamad Chalouchi <65370748+abdessamad-chalouchi@users.noreply.github.com>
2025-05-14 14:32:33 +0100

    Time: 1 ms (99.68%), Space: 42.4 MB (51.18%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,33 +1,21 @@
 class Solution {
     public String addBinary(String a, String b) {
         StringBuilder result = new StringBuilder();
-        int tmp = 0;
+        int carry = 0; // To track the carry
         int i = a.length() - 1, j = b.length() - 1;
-        while (i >= 0 || j >= 0) {
-            int r = 0;
-            if (i < 0) {
-                r = (b.charAt(j) - '0') + tmp;
-            } else if (j < 0) {
-                r = (a.charAt(i) - '0') + tmp;
-            } else {
-                r = (a.charAt(i) - '0') + (b.charAt(j) - '0') + tmp;
-            }
-            if (r == 2) {
-                tmp = 1;
-                result.insert(0, "0");
-            } else if (r == 3) {
-                tmp = 1;
-                result.insert(0, "1");
-            } else {
-                tmp = 0;
-                result.insert(0, r);
-            }
-            j--;
-            i--;
+
+        // Process both strings from the end
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int digitA = (i >= 0) ? a.charAt(i) - '0' : 0; // Get digit from 'a' or 0 if out of bounds
+            int digitB = (j >= 0) ? b.charAt(j) - '0' : 0; // Get digit from 'b' or 0 if out of bounds
+
+            int sum = digitA + digitB + carry; // Add digits and carry
+            carry = sum / 2; // Update carry (1 if sum >= 2, else 0)
+            result.append(sum % 2); // Append the remainder (0 or 1) to result
+
+            i--; // Move to the next digit in 'a'
+            j--; // Move to the next digit in 'b'
         }
-        if (tmp != 0) {
-            result.insert(0, tmp);
-        }
-        return result.toString();
+        return result.reverse().toString();
     }
 }
