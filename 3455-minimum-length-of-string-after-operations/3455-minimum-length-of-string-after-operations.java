e9c7dfc4324aed044b7c7d1b9b76e0446ffc154f
Abdessamad Chalouchi <65370748+abdessamad-chalouchi@users.noreply.github.com>
2025-07-26 16:02:13 +0100

    Time: 9 ms (95.16%), Space: 46.6 MB (37.36%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
@@ -1,17 +1,35 @@
 class Solution {
+    // public int minimumLength(String s) {
+    //     int n = s.length();
+    //     if (n <= 2) {
+    //         return n;
+    //     }
+    //     HashMap<Character, Integer> charCount = new HashMap<>();
+    //     for (char c : s.toCharArray()) {
+    //         charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+    //         if (charCount.get(c) > 2) {
+    //             n = n - 2;
+    //             charCount.put(c, charCount.get(c) - 2);
+    //         }
+    //     }
+    //     return n;
+    // }
     public int minimumLength(String s) {
-        int n = s.length();
-        if (n <= 2) {
-            return n;
+        int arr[]= new int[26];
+
+
+        for(int i=0;i<s.length();i++){
+            char ch= s.charAt(i);
+
+            arr[ch-97]++;
         }
-        HashMap<Character, Integer> charCount = new HashMap<>();
-        for (char c : s.toCharArray()) {
-            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
-            if (charCount.get(c) > 2) {
-                n = n - 2;
-                charCount.put(c, charCount.get(c) - 2);
+
+        int count=0;
+        for(int i=0;i<26;i++){
+            if(arr[i]!=0){
+            count+=(arr[i]%2==0) ? 2 :1;
             }
         }
-        return n;
+        return count;
     }
 }
