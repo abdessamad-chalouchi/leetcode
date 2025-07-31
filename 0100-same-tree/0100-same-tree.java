bf4d8fc56998ce7ebe915c3e599cbfbbd609d8df
Abdessamad Chalouchi <65370748+abdessamad-chalouchi@users.noreply.github.com>
2025-07-31 19:44:03 +0100

    Time: 0 ms (100%), Space: 41 MB (69.44%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -18,8 +18,9 @@ class Solution {
         if(p == null && q == null) return true;
         else if (p == null || q == null) return false;
         if (p.val != q.val) return false;
-        if(!isSameTree(p.left, q.left)) return false;
-        if(!isSameTree(p.right, q.right)) return false;
-        return true;
+        // if(!isSameTree(p.left, q.left)) 
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        // if(!isSameTree(p.right, q.right)) return false;
+        // return true;
     }
 }
