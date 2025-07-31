fe81494445a1251a09f8b1bdee5aa7feb385f7aa
Abdessamad Chalouchi <65370748+abdessamad-chalouchi@users.noreply.github.com>
2025-07-31 20:37:20 +0100

    Time: 0 ms (100%), Space: 42.8 MB (48.69%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -18,25 +18,25 @@ class Solution {
     /*
     * each time we check the right and left and get the max value berween theme plus 1 for first one it will goes to max left and right and when we will be on null will start adding and return to prevouis one
     */
-    // public int maxDepth(TreeNode root) {
-    //     if(root == null) return 0;    
-    //     return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
-    // }
-    // option 2 BFS
     public int maxDepth(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        if (root==null) return 0;
-        q.add(root);
-        int depth = 0;
-        while(!q.isEmpty()){
-            int levelSize = q.size();
-            depth++;
-            for(int i = 0;i<levelSize;i++){
-                TreeNode curr = q.poll();
-                if(curr.left!=null) q.add(curr.left);
-                if(curr.right!=null) q.add(curr.right);
-            }
-        }
-        return depth;
+        if(root == null) return 0;    
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
+    // option 2 BFS(Breadth-First Search)
+    // public int maxDepth(TreeNode root) {
+    //     Queue<TreeNode> q = new LinkedList<>();
+    //     if (root==null) return 0;
+    //     q.add(root);
+    //     int depth = 0;
+    //     while(!q.isEmpty()){
+    //         int levelSize = q.size();
+    //         depth++;
+    //         for(int i = 0;i<levelSize;i++){
+    //             TreeNode curr = q.poll();
+    //             if(curr.left!=null) q.add(curr.left);
+    //             if(curr.right!=null) q.add(curr.right);
+    //         }
+    //     }
+    //     return depth;
+    // }
 }
