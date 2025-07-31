9066b9f925c33fa4dfa77a7a6cd5d75912c84f2b
Abdessamad Chalouchi <65370748+abdessamad-chalouchi@users.noreply.github.com>
2025-07-31 16:06:25 +0100

    Time: 3 ms (89.86%), Space: 45.8 MB (20.41%) - LeetHub

--- 0950-x-of-a-kind-in-a-deck-of-cards/0950-x-of-a-kind-in-a-deck-of-cards.java
+++ 0950-x-of-a-kind-in-a-deck-of-cards/0950-x-of-a-kind-in-a-deck-of-cards.java
@@ -3,13 +3,20 @@ class Solution {
         return b == 0 ? a : gcd(b, a%b);
     }
     public boolean hasGroupsSizeX(int[] deck) {
-        HashMap<Integer, Integer> m = new HashMap<>();
-        for (int i = 0; i < deck.length; i++) {
-            m.put(deck[i], m.getOrDefault(deck[i], 0) + 1);
+        int[] freq = new int[10000];
+        for(int card : deck){
+            freq[card]++;
         }
-        int gcd = m.values().iterator().next();
-        for (int v : m.values()) {
-            gcd = gcd(gcd, v);
+
+        // HashMap<Integer, Integer> m = new HashMap<>();
+        // for (int i = 0; i < deck.length; i++) {
+        //     m.put(deck[i], m.getOrDefault(deck[i], 0) + 1);
+        // }
+        int gcd = 0;
+        for (int v : freq) {
+            if(v > 0){
+                gcd = gcd(gcd, v);
+            }
         }
         return gcd > 1;
     }
