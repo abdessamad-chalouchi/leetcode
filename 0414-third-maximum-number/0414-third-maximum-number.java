f6c4df03a0753ee54077391491401ebc54c77cf1
Abdessamad Chalouchi <65370748+abdessamad-chalouchi@users.noreply.github.com>
2025-08-03 15:23:22 +0100

    Time: 1 ms (97.46%), Space: 42.8 MB (89.03%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        long first=Long.MIN_VALUE, second=Long.MIN_VALUE, third=Long.MIN_VALUE;
+        if(nums.length < 3) return Math.max(nums[0], nums[nums.length-1]);
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] != first && nums[i] != second && nums[i] != third){
+                if(nums[i]>first){
+                    long tmp = second;
+                    second = first;
+                    third = tmp;
+                    first = nums[i];
+                }
+                else if (nums[i] > second){
+                    third = second;
+                    second = nums[i];
+                }
+                else if (nums[i] > third)
+                    third = nums[i];
+            }
+            
+        }
+        if (third == Long.MIN_VALUE) return (int)first;
+        return (int)third;
+    }
+}
