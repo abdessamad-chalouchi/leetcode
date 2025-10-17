d25d1a33595c4f0f7ae6f149ce9d3e8dc7990b96
Abdessamad Chalouchi <65370748+abdessamad-chalouchi@users.noreply.github.com>
2025-10-17 19:24:21 +0100

    Time: 13 ms (39.86%), Space: 45.1 MB (22.21%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -8,9 +8,9 @@ class Solution {
         // s = s.toLowerCase();
         int left=0, right=s.length()-1;
         while(left<=right){
-            if(s.charAt(left) != s.charAt(right)) return false;
-            left++;
-            right--;
+            if(s.charAt(left++) != s.charAt(right--)) return false;
+            // left++;
+            // right--;
         }
         return true;
         // return s.equals(new StringBuilder(s).reverse().toString());
