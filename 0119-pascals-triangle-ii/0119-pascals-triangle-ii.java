7774e988a25417dceff923d3e41437d3c9d491e5
Abdessamad Chalouchi <65370748+abdessamad-chalouchi@users.noreply.github.com>
2025-10-17 17:45:37 +0100

    Time: 1 ms (71.61%), Space: 41.6 MB (6.01%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -9,12 +9,12 @@ class Solution {
             List<Integer> listTmp = new ArrayList<>();
             listTmp.add(1);
             for (int j = 1; j < i - 1; j++) {
-                int tmp = res.get(res.size() - 1).get(j) + res.get(res.size() - 1).get(j - 1);
+                int tmp = list.get(j) + list.get(j - 1);
                 listTmp.add(tmp);
             }
             listTmp.add(1);
-            res.add(listTmp);
+            list = listTmp;
         }
-        return res.getLast();
+        return list;
     }
 }
